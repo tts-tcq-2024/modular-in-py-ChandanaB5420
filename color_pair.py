bb4fc3b3e1699412edf9e9000203716fc2f7cf31
ChandanaB5420 <chandanac653@gmail.com>
2024-09-25 11:51:22 +0530

    Update color_pair.py

--- color_pair.py
+++ color_pair.py
@@ -2,9 +2,13 @@
 
 from color_data import MAJOR_COLORS, MINOR_COLORS
 def get_color_from_pair_number(pair_number):
-    zero_based_pair_number = pair_number - 1    major_index = zero_based_pair_number // len(MINOR_COLORS)
+    zero_based_pair_number = pair_number - 1    
+    major_index = zero_based_pair_number // len(MINOR_COLORS)
     minor_index = zero_based_pair_number % len(MINOR_COLORS)
     return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
+    
 def get_pair_number_from_color(major_color, minor_color):
-    major_index = MAJOR_COLORS.index(major_color)    minor_index = MINOR_COLORS.index(minor_color)    return major_index * len(MINOR_COLORS) + minor_index + 1def color_pair_to_string(major_color, minor_color):
+    major_index = MAJOR_COLORS.index(major_color)    
+    minor_index = MINOR_COLORS.index(minor_color)    
+    return major_index * len(MINOR_COLORS) + minor_index + 1def color_pair_to_string(major_color, minor_color):
     return f'{major_color} {minor_color}'
