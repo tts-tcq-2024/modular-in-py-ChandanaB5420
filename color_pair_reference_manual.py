cf338e30f6ec1ae705739eafc7fd8c6a03964eb1
ChandanaB5420 <chandanac653@gmail.com>
2024-09-27 11:35:29 +0530

    Update color_pair_reference_manual.py

--- color_pair_reference_manual.py
+++ color_pair_reference_manual.py
@@ -5,8 +5,10 @@ from color_pair import get_color_from_pair_number, color_pair_to_string
 def generate_reference_manual():
     manual = []    
     for pair_number in range(1, 26):
+        
         major, minor = get_color_from_pair_number(pair_number)        
         manual.append(f'{pair_number}: {color_pair_to_string(major, minor)}')
+        
     return "\n".join(manual)
 
 
