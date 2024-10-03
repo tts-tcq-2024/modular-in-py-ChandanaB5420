5da3fe20ff108378a8e6c5c435a218cb24b71a74
ChandanaB5420 <chandanac653@gmail.com>
2024-10-03 10:46:11 +0530

    Update generate_reference_manual.py

--- generate_reference_manual.py
+++ generate_reference_manual.py
@@ -1,6 +1,6 @@
-# generate_reference_manual.py
+# main.py
 
 from color_pair_reference_manual import generate_reference_manual
 
-if __name__ == '__generate_reference_manual__':
+if __name__ == '__main__':
     generate_reference_manual()
